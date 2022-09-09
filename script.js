4ff069494e031acd7db96acf66380fb61333e4ea
ctrl-Alx-del <electromanati@gmail.com>
2022-09-09 13:53:37 +0200

    Billeder vises nu fra rest db

--- script.js
+++ script.js
@@ -7,15 +7,30 @@
 const endpoint = "https://conspirapedia-e82d.restdb.io/rest/conspirapedia";
 const apikeys = {
     headers : {
-        "x-apikey" : "1abc4f79c8afb63932d27ba9494bdcac3f866",
-    }
+        "x-apikey" : "631b232cfdc15b0265f1727b",
+    }   
 }
 
 
 async function hentData() {
     const response = await fetch(endpoint, apikeys);
     const teorier  = await response.json();
-console.log("hentet");
+    visTeori(teorier);
 }
 
 hentData();
+
+
+function visTeori(teorier) {
+    
+const main = document.querySelector("main");
+const template = document.querySelector("template").content;
+
+teorier.forEach(teori => {
+ 
+    const klon = template.cloneNode(true);
+    klon.querySelector(".billeder").src = "images/" + teori.Billednavn;
+    main.appendChild(klon);
+});
+
+}
