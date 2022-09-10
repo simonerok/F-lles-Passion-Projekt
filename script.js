fcc2ad8e679383c1e03cef136e28dbcb306beb91
ctrl-Alx-del <electromanati@gmail.com>
2022-09-10 18:03:34 +0200

    filtrering done

--- script.js
+++ script.js
@@ -2,7 +2,8 @@
 
 
 
-
+let filter = "alle";
+let teorier;
 
 const endpoint = "https://conspirapedia-e82d.restdb.io/rest/conspirapedia";
 const apikeys = {
@@ -14,7 +15,7 @@ const apikeys = {
 
 async function hentData() {
     const response = await fetch(endpoint, apikeys);
-    const teorier  = await response.json();
+    teorier = await response.json();
     visTeori(teorier);
 }
 
@@ -22,17 +23,32 @@ hentData();
 
 
 function visTeori(teorier) {
-    
 const section = document.querySelector("section");
 const template = document.querySelector("template").content;
 
+section.innerHTML = ""; //visker tavlen ren først
+
 teorier.forEach(teori => {
- 
     const klon = template.cloneNode(true);
-    klon.querySelector(".billeder").src = "images/" + teori.Billednavn;
-    klon.querySelector("h2").textContent = teori.Overskrift;
-    klon.querySelector("p").textContent = teori.Korttekst;
-    section.appendChild(klon);
+    if(filter == "alle" || filter == teori.kategori){
+        klon.querySelector(".billeder").src = "images/" + teori.Billednavn;
+        klon.querySelector("h2").textContent = teori.Overskrift;
+        klon.querySelector("p").textContent = teori.Korttekst;
+        section.appendChild(klon);
+    }
 });
 
+}
+
+const knapperne = document.querySelectorAll("button");
+
+knapperne.forEach(knap => {
+knap.addEventListener("click", vaelger);
+});
+
+function vaelger(){
+    filter = this.dataset.kategori;
+    visTeori(teorier);
+    document.querySelector(".valgt").classList.remove("valgt");
+    this.classList.add("valgt");
 }
