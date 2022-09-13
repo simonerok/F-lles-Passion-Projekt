df0688b5109076acfa9f8a606491fa9e2100e40a
ctrl-Alx-del <electromanati@gmail.com>
2022-09-13 11:23:43 +0200

    merging

--- script.js
+++ script.js
@@ -1,25 +1,63 @@
+
+
+
+
+let filter = "alle";
+let teorier;
+
+//url'en til vores rest db og vores api key til at få adgang til indholdet
 const endpoint = "https://conspirapedia-e82d.restdb.io/rest/conspirapedia";
 const apikeys = {
-  headers: {
-    "x-apikey": "631b232cfdc15b0265f1727b",
-  },
-};
+    headers : {
+        "x-apikey" : "631b232cfdc15b0265f1727b",
+    }   
+}
 
+//en asynkron funktion der henter json ned fra rest db og laver den om til noget man kan bruge i vscode
 async function hentData() {
-  const response = await fetch(endpoint, apikeys);
-  const teorier = await response.json();
-  visTeori(teorier);
+    const response = await fetch(endpoint, apikeys);
+    teorier = await response.json();
+    visTeori(teorier);
 }
 
 hentData();
 
+// viser alle vores konspirationsteorier i html'en
 function visTeori(teorier) {
-  const section = document.querySelector("section");
-  const template = document.querySelector("template").content;
+const section = document.querySelector("section");
+const template = document.querySelector("template").content;
 
-  teorier.forEach((teori) => {
+section.innerHTML = ""; //visker tavlen ren først inden der sættes mere ind i html
+
+// et for each loop der kører igennem alt json og kloner det ind i html elementerne. 
+teorier.forEach(teori => { 
     const klon = template.cloneNode(true);
-    klon.querySelector(".billeder").src = "images/" + teori.Billednavn;
-    section.appendChild(klon);
-  });
+    if(filter == "alle" || filter == teori.kategori){ //filtrerer hvad der skal vises alt efter hvad for en knap man har klikket på
+        klon.querySelector("article").addEventListener("click", ()=> {visEnkelt(teori._id)})
+        klon.querySelector(".billeder").src = "images/" + teori.Billednavn;
+        klon.querySelector("h2").textContent = teori.Overskrift;
+        klon.querySelector("p").textContent = teori.Korttekst;
+        section.appendChild(klon);
+    }
+});
+
+}
+
+const knapperne = document.querySelectorAll("button");
+//et for each loop der gør at man kan klikke på alle knapperne
+knapperne.forEach(knap => {
+knap.addEventListener("click", vaelger);
+});
+
+//en funktion der sætter filter variablen til det dataset i knappen man har klikket på og putter en klasse valgt der highlighter knappen
+function vaelger(){
+    filter = this.dataset.kategori;
+    visTeori(teorier);
+    document.querySelector(".valgt").classList.remove("valgt");
+    this.classList.add("valgt");
+}
+
+//en funktion der ændrer hvor man havner henne på sitet vha. id'et fra json-elementerne
+function visEnkelt(id){
+    location.href ="singleview.html?id="+id;
 }
